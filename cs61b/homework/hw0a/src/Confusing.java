24e55fd7d1624832fe0506875974e5ae8aca80c2
shaneyale <zeta186012@gmail.com>
2025-05-16 14:53:03 +0800

    做完了hw0a

---
+++ cs61b/homework/hw0a/src/Confusing.java
@@ -0,0 +1,47 @@
+/**
+ * Exercise sourced from Practice-It by the University of Washington.
+ * Original problems available at: https://practiceit.cs.washington.edu/
+ *
+ * @author Erik Kizior
+ */
+
+// TODO: What is the output of the following program?
+public class Confusing {
+    public static void method1() {
+        System.out.println("I am method 1.");
+    }
+    public static void method2() {
+        method1();
+        System.out.println("I am method 2.");
+    }
+    public static void method3() {
+        method2();
+        System.out.println("I am method 3.");
+        method1();
+    }
+
+    public static void main(String[] args) {
+        method1();
+        method3();
+        method2();
+        method3();
+    }
+}
+
+/* Before running the code, type your answer below.
+
+TODO: Write output here
+
+I am method 1.
+I am method 1.
+I am method 2.
+I am method 3.
+I an method 1.
+I am method 1.
+I am method 2.
+I am method 1.
+I am method 2.
+I am method 3.
+I am method 1.
+
+Then, click the green play button to check your work. */
---
+++ cs61b/homework/hw0a/src/JavaExercises.java
@@ -0,0 +1,96 @@
+/**
+ * Skeleton file for HW0A.
+ * Exercises sourced from Practice-It by the University of Washington.
+ * Original problems available at: https://practiceit.cs.washington.edu/
+ *
+ * @author Erik Kizior
+ */
+public class JavaExercises {
+
+    /**
+     * Prints a right-aligned triangle of stars ('*') with 5 lines.
+     * The first row contains 1 star, the second 2 stars, and so on.
+     */
+    public static void starTriangle() {
+        // TODO: Fill in this function
+        for (int i = 1; i <= 5; i++){
+            for (int j = 5 - i; j >=1; j--){
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++){
+                System.out.print('*');
+            }
+
+            System.out.println();
+
+        }
+    }
+
+    /**
+     * Prints each character of a given string followed by its reverse index.
+     * Example: printIndexed("hello") -> h4e3l2l1o0
+     */
+    public static void printIndexed(String s) {
+        // TODO: Fill in this function
+        for (int i = 0; i < s.length(); i++){
+            int sum = s.length() - 1;
+            System.out.print(s.charAt(i) + " " + (s.length() - 1 - i));
+        }
+        System.out.println();
+    }
+
+    /**
+     * Returns a new string where each character of the given string is repeated twice.
+     * Example: stutter("hello") -> "hheelllloo"
+     */
+    public static String stutter(String s) {
+        // TODO: Fill in this function
+        String res = "";
+        for (int i = 0; i < s.length(); i++){
+            res = res + s.charAt(i) + s.charAt(i);
+        }
+
+        return res;
+    }
+
+    /**
+     * Determines the quadrant of a Cartesian coordinate (x, y).
+     * Returns:
+     *   1 for the first quadrant (x > 0, y > 0),
+     *   2 for the second quadrant (x < 0, y > 0),
+     *   3 for the third quadrant (x < 0, y < 0),
+     *   4 for the fourth quadrant (x > 0, y < 0),
+     *   0 if the point lies on an axis.
+     */
+    public static int quadrant(int x, int y) {
+        // TODO: Fill in this function
+        if (x > 0 && y > 0){
+            return 1;
+        }
+
+        if (x < 0 && y > 0){
+            return 2;
+        }
+
+        if (x < 0 && y < 0){
+            return 3;
+        }
+
+        if (x > 0 && y < 0){
+            return 4;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        starTriangle();
+        printIndexed("hello");
+        System.out.println(stutter("hello"));
+        System.out.println(quadrant(3, 4));  // Output: 1
+        System.out.println(quadrant(-3, 4)); // Output: 2
+        System.out.println(quadrant(-3, -4));// Output: 3
+        System.out.println(quadrant(3, -4)); // Output: 4
+        System.out.println(quadrant(0, 5));  // Output: 0
+    }
+}
