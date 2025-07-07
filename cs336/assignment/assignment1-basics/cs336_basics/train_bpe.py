6b36f0e97f44212f988993360b0d2c156d46c502
shaneyale <shaneyale@mail.hfut.edu.cn>
2025-07-07 18:43:40 +0800

    完成了train_bpe.py

---
+++ cs336/assignment/assignment1-basics/cs336_basics/train_bpe.py
@@ -0,0 +1,39 @@
+# train_dpe.py
+import os
+import sys
+import pickle
+import pathlib
+from test.adapters import run_train_bpe
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__), ".."))
+
+DATA_DIR = pathlib.Path(__file__).resolve().parent.parent / "data"
+INPUT_PATH = os.path.join(DATA_DIR, "TinyStoriesV2-GPT4-train.txt")
+
+# Tokenizer的保存路径
+TOKENIZER_DIR = pathlib.Path(__file__).resolve().parent.parent / "tokenizer"
+VOCAB_PATH = os.path.join(TOKENIZER_DIR, "tinystories_bpe_vocab.pkl")
+MERGES_PATH = os.path.join(TOKENIZER_DIR, "tinystories_bpe_merges.pkl")
+
+# 训练参数
+vocab_size = 10_000
+special_tokens = ["<|endoftext|>"]
+
+# 接下来是训练的部分
+vocab, merges = run_train_bpe(
+    input_path = INPUT_PATH,
+    vocab_size = vocab_size,
+    special_tokens = special_tokens
+)
+
+# 然后是序列化到磁盘
+os.makedirs(TOKENIZER_DIR, exist_ok=True)
+with open(VOCAB_PATH, "wb") as f:
+    pickle.dump(vocab, f)
+with open(MERGES_PATH, "wb") as f :
+    pickle.dump(merges, f)
+
+# 然后是统计最长的token
+longest_token = max(vocab.values(), key = len)
+print("最长的token：", longest_token, "长度：", len(longest_token))
+
+
